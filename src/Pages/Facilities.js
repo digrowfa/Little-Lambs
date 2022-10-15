3ad49c2a5168dee3e30c5b2560532db2678f4740
Himanshu <sharmagi1998@gmail.com>
2022-10-15 11:29:26 +0530

    changes

--- src/Pages/Facilities.js
+++ src/Pages/Facilities.js
@@ -1,12 +1,14 @@
 import Navigation from './Includes/Navigation/Navigation'
 import Footer from './Includes/Footer/Footer'
 import Header from './FacilitiesContent/Header'
+import FacilitiesBody from './FacilitiesContent/FacilitiesBody'
 
 const Facilities = () => {
     return(
         <>
             <Navigation />
             <Header />
+            <FacilitiesBody />
             <Footer />
         </>
     )
--- src/Pages/FacilitiesContent/Header.js
+++ src/Pages/FacilitiesContent/Header.js
@@ -1,4 +1,8 @@
-
+import category1 from "../../img/home/category1.webp"
+import category2 from "../../img/home/category2.webp"
+import category3 from "../../img/home/category3.webp"
+import category4 from "../../img/home/category4.webp"
+import category5 from "../../img/home/category5.webp"
 
 const Header = () => {
     return (
@@ -14,7 +18,37 @@ const Header = () => {
                                 </div>
                             </div>
                         </div>
-                        
+                        <div className="row mt-5">
+                            <div className="LLNCategoryName">
+                                <ul>
+                                    <li>
+                                        <div className="LLNCategoryCircle">
+                                            <img src={category1} alt="category" className="img-fluid" />
+                                        </div>
+                                    </li>
+                                    <li>
+                                        <div className="LLNCategoryCircle">
+                                            <img src={category2} alt="category" className="img-fluid" />
+                                        </div>
+                                    </li>
+                                    <li>
+                                        <div className="LLNCategoryCircle">
+                                            <img src={category3} alt="category" className="img-fluid" />
+                                        </div>
+                                    </li>
+                                    <li>
+                                        <div className="LLNCategoryCircle">
+                                            <img src={category4} alt="category" className="img-fluid" />
+                                        </div>
+                                    </li>
+                                    <li>
+                                        <div className="LLNCategoryCircle">
+                                            <img src={category5} alt="category" className="img-fluid" />
+                                        </div>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </section>
