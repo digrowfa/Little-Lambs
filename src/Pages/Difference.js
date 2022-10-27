96b2a79f42d5d1f9338083b78b381df1057e2360
Himanshu <sharmagi1998@gmail.com>
2022-10-27 14:39:27 +0530

    Difference page responsive

--- src/Pages/Difference.js
+++ src/Pages/Difference.js
@@ -1,12 +1,14 @@
 import Navigation from './Includes/Navigation/Navigation'
 import Footer from './Includes/Footer/Footer'
 import Header from "./DifferenceContent/Header"
+import MobileNavigation from './Includes/MobileNavigation/MobileNavigation'
 import DifferenceBody from './DifferenceContent/DifferenceBody'
 
 const Difference = () => {
     return(
         <>
             <Navigation />
+            <MobileNavigation />
             <Header />
             <DifferenceBody />
             <Footer />
