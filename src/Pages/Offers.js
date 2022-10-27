918076662659d4ed3fcd36fa8ce176cfcf2c2edf
Himanshu <sharmagi1998@gmail.com>
2022-10-27 15:02:25 +0530

    offer page responsive

--- src/Pages/Offers.js
+++ src/Pages/Offers.js
@@ -1,11 +1,13 @@
 import LLNOffers from "./DifferenceContent/LLNOffers"
 import Navigation from './Includes/Navigation/Navigation'
 import Footer from './Includes/Footer/Footer'
+import MobileNavigation from "./Includes/MobileNavigation/MobileNavigation"
 
 const Offers = () => {
     return(
         <>
             <Navigation />
+            <MobileNavigation />
             <LLNOffers />
             <Footer />
         </>
