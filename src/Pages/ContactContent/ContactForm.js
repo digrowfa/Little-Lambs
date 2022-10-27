cb75b988651697fdb6bbc485d0909396d22539fc
Himanshu <sharmagi1998@gmail.com>
2022-10-27 15:31:14 +0530

    location map change

--- src/Pages/ContactContent/ContactForm.js
+++ src/Pages/ContactContent/ContactForm.js
@@ -55,7 +55,7 @@ const ContactForm = () => {
                                         </div>
                                     </form>
                                     <div className="contactMap">
-                                        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3499.580295554852!2d77.06806671549629!3d28.70219898767422!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x390d069d72fc5099%3A0xb6ecd829efcd7e32!2sKarn%20Mahashwari%20Stationary%20And%20Gift%20Store!5e0!3m2!1sen!2sin!4v1665640444311!5m2!1sen!2sin" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+                                    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3608.7431108652836!2d55.2925325150104!3d25.245575983873746!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x3ec1946262012e6f!2zMjXCsDE0JzQ0LjEiTiA1NcKwMTcnNDEuMCJF!5e0!3m2!1sen!2sin!4v1666863547094!5m2!1sen!2sin" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
                                     </div>
                                 </div>
                             </div>
