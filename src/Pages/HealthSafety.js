a393793d4949391c90dcc49d0d0071ce7009cad3
Himanshu <sharmagi1998@gmail.com>
2022-10-27 15:07:01 +0530

    HealthSafety page responsive

--- src/Pages/HealthSafety.js
+++ src/Pages/HealthSafety.js
@@ -1,12 +1,14 @@
 import Header from "./HealthSafetyContent/Header"
 import HealthSafetyBody from "./HealthSafetyContent/HealthSafetyBody"
 import Footer from "./Includes/Footer/Footer"
+import MobileNavigation from "./Includes/MobileNavigation/MobileNavigation"
 import Navigation from "./Includes/Navigation/Navigation"
 
 const HealthSafety = () => {
     return(
         <>
             <Navigation />
+            <MobileNavigation />
             <Header />
             <HealthSafetyBody />
             <Footer />
