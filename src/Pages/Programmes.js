e080586f99cd2dd6354f93a4d644d7ee3d4a82c6
Himanshu <sharmagi1998@gmail.com>
2022-10-27 14:54:36 +0530

    Programmes page responsive

--- src/Pages/Programmes.js
+++ src/Pages/Programmes.js
@@ -3,11 +3,13 @@ import Navigation from './Includes/Navigation/Navigation'
 import Footer from './Includes/Footer/Footer'
 import Header from './Programmes/Header'
 import ProgramBody from './Programmes/ProgramBody'
+import MobileNavigation from './Includes/MobileNavigation/MobileNavigation'
 
 const Programmes = () => {
     return (
         <>
             <Navigation />
+            <MobileNavigation />
             <Header />
             <ProgramBody />
             <Footer />
