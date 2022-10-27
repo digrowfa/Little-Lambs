7a9e38243a2b1551161e17f873d695db0834b900
Himanshu <sharmagi1998@gmail.com>
2022-10-27 14:45:02 +0530

    Facilities page responsive

--- src/Pages/Facilities.js
+++ src/Pages/Facilities.js
@@ -2,11 +2,13 @@ import Navigation from './Includes/Navigation/Navigation'
 import Footer from './Includes/Footer/Footer'
 import Header from './FacilitiesContent/Header'
 import FacilitiesBody from './FacilitiesContent/FacilitiesBody'
+import MobileNavigation from './Includes/MobileNavigation/MobileNavigation'
 
 const Facilities = () => {
     return(
         <>
             <Navigation />
+            <MobileNavigation />
             <Header />
             <FacilitiesBody />
             <Footer />
